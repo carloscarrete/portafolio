862cb9444f95ba56dd0d7ee48dd3b00fd0cab2e7
Carlos Carrete <carloscarrete,sc@gmail.com>
2023-06-04 18:07:45 -0600

    About Me

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { About } from './components/About'
+import { Navbar } from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <h1 className='text-9xl	'>Hilos</h1>
-    </div>
+    <div className='text-gray-400 bg-gray-900 body-font'>
+      <Navbar />
+      <About />
+    </div> 
   )
 }
 
