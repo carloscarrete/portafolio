b8995228c82e3ef5bc1e995f11b0a4e946578720
Carlos Carrete <carloscarrete,sc@gmail.com>
2023-06-11 14:52:35 -0600

    changed brand color

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -21,7 +21,7 @@ export const Skills = () => {
             skills.map((skill) => (
               <div key={skill} className="p-2 sm:w-1/2 w-full">
                 <div className="bg-gray-700 rounded flex p-4 h-full items-center">
-                  <div className="text-blue-800 w-5 h-6 flex-shrink-0 mr-4">
+                  <div className="text-blue-500 w-5 h-6 flex-shrink-0 mr-4">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z" />
                     </svg>
