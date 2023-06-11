c66f1cc120cf1a2fc799ac4b13aa04744819aee7
Carlos Carrete <carloscarrete,sc@gmail.com>
2023-06-10 20:41:21 -0600

    breakpoints for Contact Component and fixed little stuff

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,71 +10,77 @@ export const Contact = () => {
     message: '',
   })
 
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(name, email, message)
-  }
-
-
   return (
     <section id='contact' className='relative'>
-      <div className='container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap sm:mx-auto md:w-4/5 xl:w-3/5'>
-        <div className='w-1/4 text-left'>
-          <h2 className='text-white text-2xl mb-5'>Contacto</h2>
-          <h3 className='font-bold text-white text-xl'>Corro electrónico:</h3>
-          <p className='mb-10'>carloscarrete.sc@gmail.com</p>
-          <div className='flex w-36 flex-col text-center mb-10'>
-              <a href="https://gitlab.com/carloscarrete.sc" target='_blank'><img src="../../public/repositories/gitlab.svg" alt="Repositorio de Gitlab" className='w-100' /></a>
+      <div className='container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap-reverse sm:mx-auto md:w-4/5 xl:w-4/5 sm:w-3/4 sm:px-0  justify-center'>
+        <div className='flex flex-col w-full'>
+
+          <div className='mt-12'>
+            <h2 className='text-white sm:text-4xl text-3xl font-medium mb-5'>Contacto</h2>
+            <h3 className='font-bold text-white text-2xl'>Corro electrónico:</h3>
+
+            <p className='text-base leading-relaxed xl:w-1/2 lg:w-3/4 mx-auto mb-8'>carloscarrete.sc@gmail.com</p>
+
+            <h3 className='font-bold text-white text-2xl mb-5'>Repositorios:</h3>
           </div>
-          <div className='flex w-36 flex-col text-center mb-10'>
-              <a href="https://github.com/carloscarrete" target='_blank'><img src="../../public/repositories/github-icon.svg" alt="Repositorio de Github" className='w-100' /></a>
-              
+
+          <div className='flex items-center justify-around sm:flex-col'>
+            <div className='flex w-full items-center flex-col text-center mb-10'>
+              <a href="https://gitlab.com/carloscarrete.sc" target='_blank'><img src="../../public/repositories/gitlab.svg" alt="Repositorio de Gitlab" className='w-24 sm:w-36 md:w-40' /></a>
+
+            </div>
+            <div className='flex w-full items-center flex-col text-center mb-10'>
+              <a href="https://github.com/carloscarrete" target='_blank'><img src="../../public/repositories/github.svg" alt="Repositorio de Github" className='w-24 sm:w-36 md:w-40' /></a>
+            </div>
           </div>
+
         </div>
+
+
         <form
           name="contact"
           method='POST'
-          className='lg:w-3/4  flex flex-col md:w-4/6 md:ml-28 w-full mt-8 md:mt-0'
+          className='w-full flex flex-col mt-12'
         >
           <input type="hidden" name="form-name" value="contact" />
-          <h2 className='text-white sm:text-4xl text-3xl font-medium mb-5'>Formulario de contacto</h2>
-          <h2 className='text-2xl font-bold mb-4'>Puedes enviar tu consulta o mensaje mediante el siguiente formulario</h2>
+          <h2 className='text-white sm:text-4xl text-3xl font-medium mb-5 text-left'>Formulario de contacto</h2>
+          <p className="text-base leading-relaxed xl:w-4/5 lg:w-3/4 w-full mb-8 text-left">
+            Utilice nuestro formulario de contacto para enviar su consulta o mensaje. Estaremos encantados de ayudarle de manera eficiente y precisa. ¡Esperamos su mensaje!          </p>
           <div className='mb-4 text-left'>
             <label htmlFor="name">Nombre</label>
-            <input 
-            type="text"
-            id='name'
-            name='name'
-            value={name}
-            onChange={onInputChange}
-            className='w-full bg-gray-800 rounded border border-gray-700 focus:border-gray-400 focus:ring-2 py-1 text-base outline-none text-gray-400 px-4 leading-6 transition-colors duration-200 ease-in-out'
+            <input
+              type="text"
+              id='name'
+              name='name'
+              value={name}
+              onChange={onInputChange}
+              className='lg:w-4/5 md:w-11/12 w-full block bg-gray-800 rounded border border-gray-700 focus:border-gray-400 focus:ring-2 py-1 text-base outline-none text-gray-400 px-4 leading-6 transition-colors duration-200 ease-in-out'
             />
           </div>
           <div className='mb-4 text-left'>
             <label htmlFor="email">Email</label>
-            <input 
-            type="email"
-            name='email'
-            id='email'
-            value={email}
-            onChange={onInputChange}
-            className='w-full bg-gray-800 rounded border border-gray-700 focus:border-gray-400 focus:ring-2 py-1 text-base outline-none text-gray-400 px-4 leading-6 transition-colors duration-200 ease-in-out'
+            <input
+              type="email"
+              name='email'
+              id='email'
+              value={email}
+              onChange={onInputChange}
+              className='lg:w-4/5 md:w-11/12 w-full block bg-gray-800 rounded border border-gray-700 focus:border-gray-400 focus:ring-2 py-1 text-base outline-none text-gray-400 px-4 leading-6 transition-colors duration-200 ease-in-out'
             />
           </div>
           <div className='mb-4 text-left'>
             <label htmlFor="message">Mensaje</label>
-            <textarea 
-            name="message" 
-            id="message" 
-            value={message}
-            onChange={onInputChange}
-            className='w-full bg-gray-800 rounded border border-gray-700 focus:border-gray-400 focus:ring-2 transition-colors duration-200 ease-in-out h-32 py-1 px-4 resize-none outline-none text-gray-400'
+            <textarea
+              name="message"
+              id="message"
+              value={message}
+              onChange={onInputChange}
+              className='lg:w-4/5 md:w-11/12 w-full block bg-gray-800 rounded border border-gray-700 focus:border-gray-400 focus:ring-2 transition-colors duration-200 ease-in-out h-32 py-1 px-4 resize-none outline-none text-gray-400 mb-3'
             />
           </div>
           <button
             type='submit'
-            className='bg-blue-800 border-0 focus:outline-none hover:bg-blue-900 text-xl rounded-xl text-white py-3 px-5'
+            className='lg:w-4/5 md:w-11/12 block bg-blue-800 border-0 focus:outline-none hover:bg-blue-900 text-xl rounded-xl text-white py-3 px-5'
           >
             Enviar mensaje
           </button>
